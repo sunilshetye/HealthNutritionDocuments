b5c94e08dcbb7ed57bb965a24f2a704088646d14
Sunil Shetye <sunilshetye@rocketmail.com>
2024-01-21 17:49:18 +0530

    ensure that the service is cleanly stopped

--- src/test/java/org/spoken_tutorial/health/elasticsearch/HealthNutritionElasticSearchApplicationTests.java
+++ src/test/java/org/spoken_tutorial/health/elasticsearch/HealthNutritionElasticSearchApplicationTests.java
@@ -1,13 +1,23 @@
 package org.spoken_tutorial.health.elasticsearch;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spoken_tutorial.health.elasticsearch.threadpool.TaskProcessingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HealthNutritionElasticSearchApplicationTests {
+    private static final Logger logger = LoggerFactory.getLogger(HealthNutritionElasticSearchApplicationTests.class);
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TaskProcessingService taskProcessingService;
+
+    @Test
+    void contextLoads() {
+        logger.info("Stopping");
+        taskProcessingService.stop();
+    }
 
 }
